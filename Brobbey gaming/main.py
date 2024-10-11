8852c7e9051ca9626dd73a285eabd9b1b046c0cb
JasonTomeij <90309523+JasonTomeij@users.noreply.github.com>
2024-10-11 12:04:26 +0200

    Test van keuze_menu

--- Brobbey gaming/main.py
+++ Brobbey gaming/main.py
@@ -18,5 +18,26 @@ def keuze_menu():
 
     print("[1] Raad het nummer spel")
     print("[2] Galgje ")
-    print("[3] Type test")
+    print("[3] Raad de quote van de speler!")
+
+keuze_menu()
+keuze = int(input("Kies jouw keuze: "))
+
+while keuze != 0:
+    if keuze == 1:
+        #Doe optie 1
+        print("Test keuze1")
+    elif keuze == 2:
+        #doe keuze 2 
+        print("Test keuze2")
+    elif keuze == 3:
+        #DOe keuze 3:
+        print("Test keuze3")
+    else:
+        print("Dit is geen keuze")
+    keuze_menu()
+    keuze = int(input("Kies jouw keuze: "))
+
+print("Dankjewel voor het spelen van Brobbey Nasi-Store")
+
 
