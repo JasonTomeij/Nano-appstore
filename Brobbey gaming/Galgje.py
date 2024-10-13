291790c1f2e9aa27ea42b05711963b92d7e1673b
JasonTomeij <90309523+JasonTomeij@users.noreply.github.com>
2024-10-13 19:57:09 +0200

    Ene laatste update - Bronnen

--- Brobbey gaming/Galgje.py
+++ Brobbey gaming/Galgje.py
@@ -47,7 +47,6 @@ def galgje_spel():
     max_foute_beurten = 6
     woord_geraden = False
 
-    # Status: spaties in het woord moeten direct zichtbaar zijn, geen _
     huidige_status = [" " if letter == " " else "_" for letter in galgje_woord]
 
     print("Welkom bij Galgje! Let op de woorden hebben te maken met XXX")
@@ -55,7 +54,6 @@ def galgje_spel():
     print(f"Het woord heeft {len(galgje_woord)} tekens (inclusief spaties).")
     time.sleep(2)
 
-    # Spel loop
     while not woord_geraden and foute_beurten < max_foute_beurten:
         print("\nHuidige status: " + " ".join(huidige_status))
         print(f"Je hebt {foute_beurten}/{max_foute_beurten} fouten gemaakt.")
@@ -94,3 +92,11 @@ def galgje_spel():
 
 if __name__ == "__main__":
     galgje_spel()
+
+
+#Bronnen 
+
+# https://docs.python.org/3/tutorial/modules.html // Dit heeft mij heel goed gelopen en dan vooral 6 en 6.1.1 Dit heeft mij If __name__ geleerd! 
+# Timesleep = https://www.datacamp.com/tutorial/python-time-sleep?utm_source=google&utm_medium=paid_search&utm_campaignid=19589720821&utm_adgroupid=157156375191&utm_device=c&utm_keyword=&utm_matchtype=&utm_network=g&utm_adpostion=&utm_creative=716160943156&utm_targetid=dsa-2218886984100&utm_loc_interest_ms=&utm_loc_physical_ms=9102671&utm_content=&utm_campaign=230119_1-sea~dsa~tofu_2-b2c_3-row-p1_4-prc_5-na_6-na_7-le_8-pdsh-go_9-nb-e_10-na_11-na-oct24&gad_source=1&gclid=CjwKCAjw3624BhBAEiwAkxgTOo9Cpb0aT_xw2c_eQ5y28NcWvyZRKgPUL5jBp3ilpj7CbwqFHXtIoBoC9HIQAvD_BwE // Onder Kop Time.sleep()
+# Voorkennis studie
+# Canvas --> Module 1 tot 9 
